feefa2520cb8c27ed4be9f0cb50253501c55a46e
cfngc4594 <cfngc4594@gmail.com>
2025-11-12 20:44:38 +0800

    feat(ui): integrate with shadcn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  ignores: ['dist', 'dist-electron', 'node_modules', 'release', 'hono/src/generated', 'README.md', 'hono/README.md'],
+  ignores: ['dist', 'dist-electron', 'node_modules', 'release', 'hono/src/generated', 'README.md', 'hono/README.md', 'src/components/ui', 'src/hooks/use-mobile.ts'],
   formatters: true,
   react: true,
 }, {
